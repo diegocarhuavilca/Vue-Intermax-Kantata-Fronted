702b386e8a9a12cc8619e2758d6aea4071b596fb
Diego.Carhuavilca <diego.carhuavilca@gmail.com>
2021-09-09 16:01:18 -0500

    first

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+    <NavBar/>
+    <router-view></router-view>
+    <MainFooter/>
+</template>
+
+<style lang="scss">
+#app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+}
+</style>
+
+<script>
+import NavBar from '@/components/Navigation/NavBar'
+import MainFooter from '@/components/Navigation/MainFooter'
+export default {
+  
+  components:{
+    NavBar,
+    MainFooter
+  }
+}
+</script>
